ee24a2515ba1d0bc05e8d80a03ce0f096543b7e4
kojiw <w33426440@gmail.com>
2026-05-30 17:52:53 +0900

    5/30 작업내용

---
+++ Assets/Scripts/TowerSpawner.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class TowerSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject towerPrefab;
+
+    public void SpawnTower(Transform tileTransform)
+    {
+        //선택한 타일의 위치에 타워 건설
+        Instantiate(towerPrefab, tileTransform.position, Quaternion.identity);
+    }
+}
